ab4ec576e80cf3c64216d34c40f8ee9aecd4b5af
Aky006 <aky.iitg006@gmail.com>
2020-09-17 03:43:03 +0530

    first commit

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import Build from './Build';
 import './App.css';
 
@@ -7,19 +7,86 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src= "logo192.png"  alt="Error 404" />
+        <p>Agrawal Sanitation</p>
+        
+        <p><button type="button">HOME</button><button type="button">ABOUT US</button><button type="button">PRODUCTS WE DEAL</button>
+        <button type="button">BRANDS WE DEAL</button><button type="button">AWARDS & CERTIFICATES</button>
+        <button type="button">CONTACT</button></p>
+        <p className="App-blue">  Welcome to Agrawal Sanitation! </p>
+      <p className="App-white"> <Build/> </p>
+      <p
+          className="App-red">
+          Products We Deal
         </p>
-        <a
+        <p> <img src= "Water Closets.jpg"  alt="Water Closets"  /> &nbsp;  <img src= "Pedestal_Basin.jpg"  alt="Pedestal_Basin" /> &nbsp;
+        <img src= "Bathroom Accessories.jpg"  alt="Bathroom Accessories" />&nbsp;  <img src= "Faucets.jpg"  alt="Faucets" /> 
+        &nbsp; <img src= "showers.jpg"  alt="showers" /> &nbsp; <img src= "cistern.jpg"  alt="cistern" />&nbsp; 
+        <img src= "Urinals.jpg"  alt="Urinals" /></p>
+        <p> <img src= "UPVC Pipes and fittings.png"  alt="UPVC Pipes and fittings" /> &nbsp; <img src= "CPVC Pipes and fittings.png"  alt="CPVC Pipes and fittings" />  &nbsp;
+        <img src= "SWR Pipes and fittings.png"  alt="SWR Pipes and fittings" /> &nbsp; <img src= "UDS Pipes and fittings.png"  alt="UDS Pipes and fittings" />  &nbsp; 
+        <img src= "water tanks.jpg"  alt="water tanks" />  &nbsp; <img src= "Adhesives.jpg"  alt="Adhesives" /> &nbsp;
+        <img src= "Kitchen sink.jpg"  alt="Kitchen sink" />  &nbsp; <img src= "Sanitary spares.jpg"  alt="Sanitary spares" />  </p>
+      <p
+          className="App-red">
+          Brands We Deal
+        </p>
+      <a
           className="App-link"
-          href="https://reactjs.org"
+          href= "https://www.hindwarehomes.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           <img src= "Hindware.jpg"  alt="Hindware" />
         </a>
-        <Build/>
+      <a
+          className="App-link"
+          href= "https://ajaypipes.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           <img src= "Ajay.jpg"  alt="Ajay Pipes" />
+        </a>
+        <a
+          className="App-link"
+          href= "https://utkarshindia.in/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           <img src= "Utkarsh.jpg"  alt="Utkarsh Pipes" />
+        </a>
+        <a
+          className="App-link"
+          href= "http://roff.in/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           <img src= "Pidilite.jpg"  alt="Pidilite Pipes" />
+        </a>
+        <a
+          className="App-link"
+          href= "http://www.gangabathfittings.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           <img src= "Ganga.png"  alt="Ganga" />
+        </a>
+        <a
+          className="App-link"
+          href= "https://www.amoghapolymers.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           <img src= "Amogha.png"  alt="Amogha" />
+        </a>
+        <p
+          className="App-red">
+          Submit Your Query:
+          </p>
+
+        <p className="App-div"> Name &nbsp;&ensp;&emsp; Email <br></br> Phone <br></br>Enter your message 
+        <br></br> <button type="button">Send</button></p>
+      
       </header>
     </div>
   );
--- web/src/Build.js
+++ web/src/Build.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 
 class Build extends Component{
     render(){
-        return <h1>Soumya Rai</h1>
+        return <h1>
+            
+
+        Since Incorporation in 2001 Under the ownership of Mr. Pankaj Kumar Agrawal, 
+        Agrawal Sanitation has distinctly
+        emerged as the preferred choice for a discerning range of individuals. <br></br>
+        We are the Authorised dealer of Ajay, Ganga, Pidilite, Watertec, Volga, etc. in Lohardaga, Our strong networking, tie-ups with
+        leading brands, customer friendly business policies, have all contributed towards
+        the success of our organization for more than 18 years. <br></br>
+        We have been providing an enriched brand experience, luxurious serenity, comfort and convenience
+        with a sublime ambiance to give you a pleasurable shopping experience and overall satisfaction.<br></br>
+        Our offered products segments are Sanitary Ware, Bath Fittings, Pipes and Fittings, Water Tank, Adhesives and sealants, etc.
+        </h1>
     }
 }
 
