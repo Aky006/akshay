2f7e6a58f4a777095773d083069de08f5f350345
Akshay Kumar <48856700+Aky006@users.noreply.github.com>
2020-09-21 00:07:44 +0530

    Update Build.js

--- src/Build.js
+++ src/Build.js
@@ -10,7 +10,7 @@ class Build extends Component{
         emerged as the preferred choice for a discerning range of individuals. <br></br>
         We are the Authorised dealer of Ajay, Ganga, Pidilite, Watertec, Volga, etc. in Lohardaga, Our strong networking, tie-ups with
         leading brands, customer friendly business policies, have all contributed towards
-        the success of our organization for more than 18 years. <br></br>
+        the success of our organization for more than </br> 18 years. <br></br>
         We have been providing an enriched brand experience, luxurious serenity, comfort and convenience
         with a sublime ambiance to give you a pleasurable shopping experience and overall satisfaction.<br></br>
         Our offered products segments are Sanitary Ware, Bath Fittings, Pipes and Fittings, Water Tank, Adhesives and sealants, etc.
