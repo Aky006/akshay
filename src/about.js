534091e14c65a090778a407cf07ff5162fb9520d
Akshay Kumar <48856700+Aky006@users.noreply.github.com>
2020-09-21 03:58:28 +0530

    first commit

--- src/about.js
+++ src/about.js
@@ -13,7 +13,7 @@ function About () {
 We provide our products in Lohardaga and also outside the valley (i.e. Chandwa, Latehar, Gumla, e.t.c.) </p>
 
 <p className="App-red">Our Core Values :</p>  
-Customer delight : <br /><i> We believe in the philosophy of <br /> “once a customer - always a customer”<br /> Once you walk through our door, you will always want to return to us for all your Sanitaryware and Pipes needs.</i><br /><br />
+Customer delight : <br /><i> We believe in the philosophy of <br /> “once a customer-always a customer”<br /> Once you walk through our door, you will always want to return to us for all your Sanitaryware and Pipes needs.</i><br /><br />
 Quality :<i> We are committed to supplying the highest quality products and strive to surpass customer expectations.</i><br /><br />
 Value for money :<i> We understand the value of money and ensure that our customers feel completely content about what they spend at Agrawal Sanitation in terms of cost and quality.</i><br /><br />
 Variety of choice :<i> We offer a host of international and national brands and constantly attempt to bring you the latest in the industry.</i>
