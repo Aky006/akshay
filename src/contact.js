c70b2a9b103279e8a97a0d1f97c3cc001833db75
Akshay Kumar <48856700+Aky006@users.noreply.github.com>
2020-11-18 10:10:29 +0530

    Update contact.js

--- src/contact.js
+++ src/contact.js
@@ -11,11 +11,17 @@ class Contact extends Component{
             You can find us here:<br /> 
              
             Agrawal Sanitation 
+            <p> 
+            Our new showroom location is easy to find: <br /> 
+            Opp. Jharkhand Gramin Bank <br /> 
+            Near Reliance Petrol Pump  <br /> 
+            Basarbari, Barwatoli <br /> 
+            Lohardaga, Jharkhand (835302)  </p> 
        <p> 
             Mahavir Chowk <br /> 
             Lohardaga, Jharkhand (835302)  </p> 
        <p> 
-            Mob No. : +91-8539073975 
+            Mob No. : +91-8539073975/ +91-9431766688
             </p>
         </address> 
      
