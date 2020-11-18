66b76bb7a46265a82a021c95de241f3b548875f5
Akshay Kumar <48856700+Aky006@users.noreply.github.com>
2020-11-18 10:34:33 +0530

    Update contact.js

--- src/contact.js
+++ src/contact.js
@@ -26,7 +26,7 @@ class Contact extends Component{
             Mahavir Chowk <br /> 
             Lohardaga, Jharkhand (835302)  <br />
        *we served at this location for over 18 years, now we have moved to a new place mentioned above  <br /> 
-         We value your patronage and look forward to your visit at our new location! 
+         <p>We value your patronage and look forward to your visit at our new location! </p>
          </p> 
       
         </address> 
