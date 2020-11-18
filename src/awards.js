9a598e44e4502d320e1de38653d61f69bb3a3ffb
Akshay Kumar <48856700+Aky006@users.noreply.github.com>
2020-11-18 13:43:18 +0530

    Update awards.js

--- src/awards.js
+++ src/awards.js
@@ -9,7 +9,7 @@ function Awards () {
     return <div> 
         <p><img src= "volga cert.jpeg"  alt="photo_1" /></p>
         <p><img src= "ajay cert.jpeg"  alt="photo_2" /></p>
-        <p><img src= "watertec cert.jpeg"  alt="photo_2" /></p>
+     
         <h4>still to be uploaded. check back soon!</h4>
   
         
